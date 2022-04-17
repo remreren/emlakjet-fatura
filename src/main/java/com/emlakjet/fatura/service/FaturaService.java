122183121fa8dcc0891a05360ecb6ff0c6eeef6f
Ritotwo <eeren881@gmail.com>
2022-04-17 21:55:07 +0300

    + configuration externalized as wanted.

--- src/main/java/com/emlakjet/fatura/service/FaturaService.java
+++ src/main/java/com/emlakjet/fatura/service/FaturaService.java
@@ -6,6 +6,7 @@ import com.emlakjet.fatura.model.Fatura;
 import com.emlakjet.fatura.repo.FaturaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -18,11 +19,14 @@ public class FaturaService {
     @Autowired
     private FaturaRepository faturaRepository;
 
+    @Value("${emlakjet.creditLimit}")
+    private BigDecimal limit;
+
     /**
      * Gets the new fatura creation request and converts it into fatura entity object.
      * If sum of previous fatura's and current fatura's of the Purchasing Specialist
-     * is greater than the given value in config file, then returns an error. Otherwise
-     * saves the new fatura to the database.
+     * is greater than the given {@link #limit} in config file, then returns an error.
+     * Otherwise, saves the new fatura to the database.
      *
      * @param faturaRequest {@link RequestNewFatura} Request to create a new fatura.
      * @return {@link Fatura} Data Access Object saved into database.
@@ -33,8 +37,8 @@ public class FaturaService {
         BigDecimal sumBefore = faturaRepository
                 .getSumOfPSFaturas(fatura.getNameOfPS(), fatura.getSurnameOfPS(), fatura.getEmailOfPS())
                 .orElse(BigDecimal.ZERO);
-        if (sumBefore.add(fatura.getAmount()).compareTo(BigDecimal.valueOf(200)) > 0)
-            throw new SumOfPSFaturasExceededLimit(sumBefore, fatura.getAmount(), BigDecimal.valueOf(200));
+        if (sumBefore.add(fatura.getAmount()).compareTo(limit) > 0)
+            throw new SumOfPSFaturasExceededLimit(sumBefore, fatura.getAmount(), limit);
         return faturaRepository.save(fatura);
     }
 
